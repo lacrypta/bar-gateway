ec3be6f235fd9c69c378cb8a3f02efbbe13443d8
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2022-10-11 13:41:07 -0300

    Instantiate ERC20Gateway with PeronioV2 address

--- contracts/BarGateway.sol
+++ contracts/BarGateway.sol
@@ -3,6 +3,16 @@ pragma solidity ^0.8.17;
 
 import {ERC20Gateway} from "lacrypta-gateway/contracts/ERC20Gateway.sol";
 
+
+address constant PERONIO_ADDRESS = 0x78a486306D15E7111cca541F2f1307a1cFCaF5C4;
+
 contract BarGateway is ERC20Gateway {
-    constructor(address _token) ERC20Gateway(_token) {}
+
+    constructor() ERC20Gateway(PERONIO_ADDRESS) {}
+
+    // To obtain message to be signed from voucher: stringifyVoucher(voucher)
+    //   The signing procedure _should_ sign the _hash_ of this message
+    //
+    // To serve a voucher (1): serveVoucher(voucher, r, s, v)
+    // To serve a voucher (2): serveVoucher(voucher, sig)
 }
