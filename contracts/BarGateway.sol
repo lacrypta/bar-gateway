9b725dd4b84fd184ad0600d31c22d8d4cf910858
Mariano Pérez Rodríguez <mariano.perez.rodriguez@gmail.com>
2022-10-20 16:17:35 -0300

    Make the Bar Gateway show a provided message

--- contracts/BarGateway.sol
+++ contracts/BarGateway.sol
@@ -1,10 +1,99 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.17;
 
-import {ERC20PermitGateway} from "@lacrypta/gateway/contracts/ERC20PermitGateway.sol";
+import {ERC20Gateway} from "@lacrypta/gateway/contracts/ERC20Gateway.sol";
+
+import {IERC20Gateway} from "@lacrypta/gateway/contracts/IERC20Gateway.sol";
 
 import {IBarGateway} from "./IBarGateway.sol";
 
-contract BarGateway is ERC20PermitGateway, IBarGateway {
-    constructor(address _peronio) ERC20PermitGateway(_peronio) {}
+contract BarGateway is ERC20Gateway, IBarGateway {
+
+    constructor(address _peronio) ERC20Gateway(_peronio) {
+        _addHandler(TRANSFER_FROM_VOUCHER_TAG, HandlerEntry({
+            message: _generateBarStyleTransferFromVoucherMessage,
+            signer: _extractTransferFromVoucherSigner,
+            execute: _executeTransferFromVoucher
+        }));
+    }
+
+    /**
+     * Retrieve the address of the underlying ERC20 token
+     *
+     * @return _erc20Token  The address of the underlying ERC20 token
+     */
+    function token() external view override(ERC20Gateway, IERC20Gateway) returns (address _erc20Token) {
+        _erc20Token = IERC20Gateway(this).token();
+    }
+
+    /**
+     * Implementation of the IERC165 interface
+     *
+     * @param interfaceId  Interface ID to check against
+     * @return  Whether the provided interface ID is supported
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(IBarGateway).interfaceId || super.supportsInterface(interfaceId);
+    }
+
+    /**
+     * Build a Bar TransferFromVoucher from the given parameters
+     *
+     * @param nonce  Nonce to use
+     * @param deadline  Voucher deadline to use
+     * @param from  Transfer origin to use
+     * @param to  Transfer destination to use
+     * @param amount  Transfer amount to use
+     * @param message  Bar message to use
+     * @return voucher  The generated voucher
+     */
+    function buildBarTransferFromVoucher(uint256 nonce, uint256 deadline, address from, address to, uint256 amount, string calldata message) external view returns (Voucher memory voucher) {
+        voucher = _buildBarTransferFromVoucher(nonce, deadline, from, to, amount, message);
+    }
+
+    /**
+     * Build a Bar TransferFromVoucher from the given parameters
+     *
+     * @param nonce  Nonce to use
+     * @param from  Transfer origin to use
+     * @param to  Transfer destination to use
+     * @param amount  Transfer amount to use
+     * @param message  Bar message to use
+     * @return voucher  The generated voucher
+     */
+    function buildBarTransferFromVoucher(uint256 nonce, address from, address to, uint256 amount, string calldata message) external view returns (Voucher memory voucher) {
+        voucher = _buildBarTransferFromVoucher(nonce, block.timestamp + 1 hours, from, to, amount, message);
+    }
+
+    /**
+     * Build a Voucher from the given parameters
+     *
+     * @param nonce  Nonce to use
+     * @param deadline  Voucher deadline to use
+     * @param from  Transfer origin to use
+     * @param to  Transfer destination to use
+     * @param amount  Transfer amount to use
+     * @param message  Bar message to use
+     * @return voucher  The generated voucher
+     */
+    function _buildBarTransferFromVoucher(uint256 nonce, uint256 deadline, address from, address to, uint256 amount, string memory message) internal pure returns (Voucher memory voucher) {
+        voucher = Voucher(
+            TRANSFER_FROM_VOUCHER_TAG,
+            nonce,
+            deadline,
+            abi.encode(TransferFromVoucher(from, to, amount)),
+            abi.encode(BarMetadata(message))
+        );
+    }
+
+    /**
+     * Generate the user-readable message from the given voucher
+     *
+     * @param voucher  Voucher to generate the user-readable message of
+     * @return message  The voucher's generated user-readable message
+     */
+    function _generateBarStyleTransferFromVoucherMessage(Voucher calldata voucher) internal view returns (string memory message) {
+        abi.decode(voucher.payload, (TransferFromVoucher));  // make sure we can decode the body
+        message = abi.decode(voucher.metadata, (BarMetadata)).message;
+    }
 }
--- contracts/IBarGateway.sol
+++ contracts/IBarGateway.sol
@@ -1,7 +1,40 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.17;
 
-import {IERC20PermitGateway} from "@lacrypta/gateway/contracts/IERC20PermitGateway.sol";
+import {IERC20Gateway} from "@lacrypta/gateway/contracts/IERC20Gateway.sol";
 
-interface IBarGateway is IERC20PermitGateway {
+interface IBarGateway is IERC20Gateway {
+    /**
+     * Metadata to use for bar purchases
+     *
+     * @custom:member message  Message to use instead of normal transferFromVoucher message
+     */
+    struct BarMetadata {
+        string message;
+    }
+
+    /**
+     * Build a Bar TransferFromVoucher from the given parameters
+     *
+     * @param nonce  Nonce to use
+     * @param deadline  Voucher deadline to use
+     * @param from  Transfer origin to use
+     * @param to  Transfer destination to use
+     * @param amount  Transfer amount to use
+     * @param message  Bar message to use
+     * @return voucher  The generated voucher
+     */
+    function buildBarTransferFromVoucher(uint256 nonce, uint256 deadline, address from, address to, uint256 amount, string calldata message) external view returns (Voucher memory voucher);
+
+    /**
+     * Build a Bar TransferFromVoucher from the given parameters
+     *
+     * @param nonce  Nonce to use
+     * @param from  Transfer origin to use
+     * @param to  Transfer destination to use
+     * @param amount  Transfer amount to use
+     * @param message  Bar message to use
+     * @return voucher  The generated voucher
+     */
+    function buildBarTransferFromVoucher(uint256 nonce, address from, address to, uint256 amount, string calldata message) external view returns (Voucher memory voucher);
 }
